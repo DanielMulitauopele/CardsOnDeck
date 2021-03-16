8ab9023599e740e80241868d13a20541d08a8bd5
Daniel Mulitauopele <37354322+DanielMulitauopele@users.noreply.github.com>
2021-03-16 02:54:42 -0600

    Update cards_on_deck.rb

--- lib/cards_on_deck.rb
+++ lib/cards_on_deck.rb
@@ -5,6 +5,6 @@ require_relative "cards_on_deck/version"
 module CardsOnDeck
   class Error < StandardError; end
 
-  require 'cards_on_deck/card'
-  require 'cards_on_deck/deck'
+  require 'cards_on_deck/gem_card'
+  require 'cards_on_deck/gem_deck'
 end
